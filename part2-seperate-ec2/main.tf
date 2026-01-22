d9ff19b05f0f1b1e5b626058ccb12faa3d9c66e9
jyothsnandk <jyothsnaadari2000@gmail.com>
2026-01-22 11:16:49 +0530

    Update main.tf

--- part2-seperate-ec2/main.tf
+++ part2-seperate-ec2/main.tf
@@ -13,14 +13,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Data source for latest Amazon Linux 2 AMI
+# Data source for latest Amazon Linux 2023 AMI
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-*-x86_64"]
   }
 }
 
